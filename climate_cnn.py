eec9dd6c356c73c969cc4cd1ee62762f5a33cc74
jordanking <king96@lakeclark.llnl.gov>
2015-07-16 10:46:26 -0700

    Bug fixes to kmeans
    ignoring all csv now

--- climate_cnn.py
+++ climate_cnn.py
@@ -70,6 +70,11 @@ class Predictor:
         self.models = [{'path': 'data/model1_train.csv', 'resolution':[192,288], 'y':0},
                         {'path': 'data/model2_train.csv', 'resolution':[128,256], 'y':1},
                         {'path': 'data/model3_train.csv', 'resolution':[160,320], 'y':2}]
+
+        # self.models = [{'path': 'small_data/model1_train.csv', 'resolution':[99,1], 'y':0},
+        #                 {'path': 'small_data/model2_train.csv', 'resolution':[99,1], 'y':1},
+        #                 {'path': 'small_data/model3_train.csv', 'resolution':[99,1], 'y':2}]
+
         self.test_data = {'path': 'data/model_test.csv', 'resolution':[72,144], 'y':[]}
         self.nb_classes = len(self.models)
 
@@ -100,12 +105,9 @@ class Predictor:
         for m in range(len(self.models)):
             print('Loading model', m+1, '...')
             # print(self.models[m])
-
             self.X[idx:idx+self.observations[m], :self.columns] = pd.read_csv(self.models[m]['path'], delimiter=',', header=0, dtype='float32').values[:,1:self.columns+1]
             self.X[idx:idx+self.observations[m], self.columns] = self.models[m]['y']
             idx += self.observations[m]
-
-        print('Normalizing data... jk')
 
         print('Shuffling data order...')
         np.random.shuffle(self.X)
@@ -119,7 +121,7 @@ class Predictor:
 
         self.y = self.X[:,[self.columns]]
         self.X = self.X[:, :self.columns]
-        print(self.y.shape, self.y)
+
         self.y = np_utils.to_categorical(self.y, self.nb_classes)
 
         # preprocess?
@@ -202,11 +204,13 @@ class Predictor:
 
             for i in range(X_test.shape[0]):
                 total += 1
-                best_class = self.find_best_class(X_test[i], X_train, y_train)
+                best_class, best_dist, next_best_class, next_best_dist, n = self.find_best_class(X_test[i], X_train, y_train)
 
                 if (best_class == y_test[i]).all():
                     correct += 1
-                print('Seen:',total, 'Correct:', correct, 'Accuracy:', correct/total)
+
+                print('Seen:',total, 'Pred:', np.argmax(best_class)+1,'Truth:',np.argmax(y_test[i])+1, 'Dist', best_dist, 'Corr:', correct, 'Acc:', correct/total)
+                print('Test Obs:', X_test[i], 'Nearest Neighbor:', X_train[n])
             
             score = correct / X_test.shape[0]
             print ('Score: ' + str(score))
@@ -222,36 +226,35 @@ class Predictor:
         model just gets better with time for now. This is pretty expensive to run.
         """
 
-        predictions = []
-        X_test = pd.read_csv(self.test_file, delimiter=',', skiprows=1, dtype='float32')
+        X_test = pd.read_csv(self.test_data['path'], delimiter=',', skiprows=1, dtype='float32').values[:,1:self.columns+1]
+        predictions = np.empty([X_test.shape[0]], dtype = 'int32')
 
-        X_train, y_train = self.X[train], self.y[train]
-
+        X_train, y_train = self.X, self.y
 
         for i in range(X_test.shape[0]):
 
-            best_class = self.find_best_class(X_test[i], X_train, y_train)
+            best_class, best_dist, next_best_class, next_best_dist, n = self.find_best_class(X_test[i], X_train, y_train)
+            # print('best class',best_class,'best_dist',best_dist)
+            predictions[i] = int(np.argmax(best_class)) + 1
+            print('ID:', i, 'Pred:', predictions[i], 'Dist:', best_dist, '2nd Pred:', int(np.argmax(next_best_class))+1, '2nd dist:', next_best_dist)
+            print('Test Obs:', X_test[i], 'Nearest Neighbor:', X_train[n])
 
-            
-            print('Seen:', i)
-        
-        score = correct / X_test.shape[0]
-        print ('Score: ' + str(score))
-        scores.append(score)
-        
-        scores = np.array(scores)
-
+        return predictions
 
     def find_best_class(self, current, X_train, y_train):
-        best_class, best_dist = 0, 3.4028235e+38
+        best_class, next_best_class, best_dist, next_best_dist, best_idx = [0,0,0], [0,0,0], 3.4028235e+38, 3.4028235e+38, 0
         for n in range(X_train.shape[0]):
             if abs(X_train[n][0] - current[0]) < 2 and abs(X_train[n][1]- current[1]) < 2:
-                dist = np.linalg.norm(X_train[n] - current)
+                # print(X_train[n])
+                dist = np.linalg.norm(X_train[n][2:] - current[2:])
                 if dist < best_dist:
+                    next_best_dist = best_dist
                     best_dist = dist
+                    next_best_class = best_class
                     best_class = y_train[n]
+                    best_idx = n
         # return np.argmax(best_class) - 1
-        return best_class
+        return best_class, best_dist, next_best_class, next_best_dist, best_idx
 
     def get_predictions(self):
         """
@@ -294,7 +297,7 @@ class Predictor:
         """
 
         if self.mode != "load":
-            print('loading data...')
+            print('Loading data...')
             self.load_data()
         
         if self.mode != 'xval' and self.mode != 'kmnn':
@@ -305,9 +308,13 @@ class Predictor:
             print('evaluating model...')
             self.cross_validate()
 
+        if self.mode == 'kmxv':
+            print('Performing k-means xval')
+            self.kmeans_xval()
+
         if self.mode == 'kmnn':
-            print('performing k-means nn')
-            self.kmeans()
+            print('Performing k-means nearest neighbor search')
+            self.save_predictions(self.kmeans())
 
         if self.mode == 'pred':
             print('training model...')
